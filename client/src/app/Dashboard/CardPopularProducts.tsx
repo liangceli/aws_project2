067de0dd64ee3b04597a350c2221baa997c28934
Liangce Li <liliangce316@gmail.com>
2025-03-13 00:37:58 +1030

    fix

--- client/src/app/Dashboard/CardPopularProducts.tsx
+++ client/src/app/Dashboard/CardPopularProducts.tsx
@@ -2,7 +2,6 @@ import { useGetDashboardMetricsQuery } from "../state/api";
 import { ShoppingBag } from "lucide-react";
 import React from "react";
 import Rating from "../(components)/Navbar/Rating";
-import Image from "next/image";
 
 const CardPopularProducts = () => {
   const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+
 import Dashboard from "@/app/Dashboard/page"
 
 export default function Home() {
